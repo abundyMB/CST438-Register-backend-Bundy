277716b0f2bf4096154ea98139ef2fb6a61e07ec
Andrew <abundy@csumb.edu>
2021-10-15 16:00:01 -0700

    Week 4 Updates

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -1,6 +1,5 @@
 package com.cst438.service;
 
-
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -24,27 +23,30 @@ public class GradebookServiceMQ extends GradebookService {
 	@Autowired
 	Queue gradebookQueue;
 	
-	
 	public GradebookServiceMQ() {
 		System.out.println("MQ grade book service");
 	}
 	
-	// send message to grade book service about new student enrollment in course
+	// Sends message to grade book service about new student enrollment in course.
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		 
-		//TODO  complete this method in homework 4
-		
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+		System.out.println("Sending rabbitmq message: "+ enrollmentDTO);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+		System.out.println("Message sent.");
 	}
 	
+	// Updates grades for each student in the course. 
 	@RabbitListener(queues = "registration-queue")
 	@Transactional
 	public void receive(CourseDTOG courseDTOG) {
 		
-		//TODO  complete this method in homework 4
-		
+		for (CourseDTOG.GradeDTO thisGrade : courseDTOG.grades)
+		{
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(thisGrade.student_email, courseDTOG.course_id);
+			enrollment.setCourseGrade(thisGrade.grade);
+			enrollmentRepository.save(enrollment);
+		}
 	}
-	
-	
-
 }
--- cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -1,11 +1,12 @@
 package com.cst438.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
 import com.cst438.domain.EnrollmentDTO;
 
-
+// This class sends a new student enrollment to the GradeBook service.
 public class GradebookServiceREST extends GradebookService {
 
 	private RestTemplate restTemplate = new RestTemplate();
@@ -20,8 +21,14 @@ public class GradebookServiceREST extends GradebookService {
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		
-		//TODO  complete this method in homework 4
-		
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+
+	    ResponseEntity<EnrollmentDTO> response = restTemplate.postForEntity("http://localhost:8081/enrollment", enrollmentDTO, null);
+	    
+		HttpStatus rc = response.getStatusCode();
+		System.out.println("HttpStatus: "+rc);
+		EnrollmentDTO returnObject = response.getBody();
+		System.out.println(returnObject);
 	}
 
 }
